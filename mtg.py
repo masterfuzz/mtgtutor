63e6283dbeedc60a01057cf66728ec930beec2e0
Robert Martens <robert.martens@bell.ca>
2014-11-17 17:25:15 -0500

    added notes

--- mtg.py
+++ mtg.py
@@ -13,5 +13,20 @@ def _get(arg):
 def search(name):
 	return _get("search/%s?start=0&limit=2" % name)
 
+def byID(uid):
+	pass
+
+
+def confer(uid):
+	# get gatherer comments and return list of card links
+	br.open("http://gatherer.wizards.com/Pages/Card/Discussion.aspx?multiverseid=%s" % uid)
+	cf = []
+	
+	for link in br.links(url_regex="Details.aspx.name"):
+		cf.append(search(link.text))
+
+	#TODO other comment pages
+	return cf
+
 
 card = search("Cranial Archive")
